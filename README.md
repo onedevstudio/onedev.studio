8e79c1af290ed9b35ab6619d68f6bb1e46b6b06c
Fernando Moreira <nandomoreira.me@gmail.com>
2017-10-13 11:14:42 -0300

    Start project with nuxtjs

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-## One dev's studio
+# OneDev studio
 
-Um estúdio de desenvolvimento web de um homem só
+> Um estúdio de desenvolvimento web de um homem só
 
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm install # Or yarn install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, checkout the [Nuxt.js docs](https://github.com/nuxt/nuxt.js).
---
+++ nuxt.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  srcDir: 'src/',
+  dev: (process.env.NODE_ENV !== 'production'),
+  head: {
+    title: 'OneDev studio',
+    htmlAttrs: {
+      lang: 'pt-BR'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { hid: 'description', name: 'description', content: 'Um estúdio de desenvolvimento web de um homem só' },
+      { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
+      { name: 'viewport', content: 'width=device-width, minimum-scale=1.0, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes' },
+      { name: 'og:locale', content: 'pt_BR' },
+      { name: 'googlebot', content: 'index,follow' },
+      { name: 'robots', content: 'index,follow,noodp' },
+      { name: 'theme-color', content: '#0860a8' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:site', content: '@oseunando' },
+      { name: 'country', content: 'Brazil' },
+      { name: 'revisit-after', content: '7 days' },
+      { name: 'mobile-web-app-capable', content: 'yes' },
+      { name: 'application-name', content: 'Fernando Moreira | Front-end / UX Designer' },
+      { name: 'generator', content: 'Visual Studio Code v1.17' }
+    ],
+    link: [
+      { rel: 'me', href: 'nandomoreira.me@gmail.com' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  loading: { color: '#3B8070' },
+  build: {
+    extend (config, ctx) {
+      if (ctx.dev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}

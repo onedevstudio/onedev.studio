74c32af418b4b9c8656e6842985a29dc892ed978
Fernando Moreira <nandomoreira.me@gmail.com>
2017-10-13 12:50:59 -0300

    Add Meta for Facebook and Twitter

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,26 +2,36 @@ module.exports = {
   srcDir: 'src/',
   dev: (process.env.NODE_ENV !== 'production'),
   head: {
-    title: 'OneDev studio',
+    title: 'OneDev studio - Um estúdio de desenvolvimento web de um homem só',
     htmlAttrs: {
       lang: 'pt-BR'
     },
     meta: [
       { charset: 'utf-8' },
-      { hid: 'description', name: 'description', content: 'Um estúdio de desenvolvimento web de um homem só' },
+      { hid: 'description', name: 'description', content: 'Um estúdio de desenvolvimento web de um homem só em Curitiba Paraná, desenvolvimento JavaScript e WordPress' },
       { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
       { name: 'viewport', content: 'width=device-width, minimum-scale=1.0, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes' },
       { name: 'og:locale', content: 'pt_BR' },
       { name: 'googlebot', content: 'index,follow' },
       { name: 'robots', content: 'index,follow,noodp' },
       { name: 'theme-color', content: '#4DBA87' },
-      { name: 'twitter:card', content: 'summary_large_image' },
-      { name: 'twitter:site', content: '@oseunando' },
       { name: 'country', content: 'Brazil' },
       { name: 'revisit-after', content: '7 days' },
       { name: 'mobile-web-app-capable', content: 'yes' },
       { name: 'application-name', content: 'Fernando Moreira | Front-end / UX Designer' },
-      { name: 'generator', content: 'Visual Studio Code v1.17' }
+      { name: 'generator', content: 'Visual Studio Code v1.17' },
+      // Meta for Facebook
+      { property: 'og:title', content: 'OneDev studio - Um estúdio de desenvolvimento web de um homem só' },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:image', content: '/share.png' },
+      { property: 'og:description', content: 'Um estúdio de desenvolvimento web de um homem só em Curitiba Paraná, desenvolvimento JavaScript e WordPress' },
+      // Meta for Twitter
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:site', content: '@OnedevStudioweb' },
+      { name: 'twitter:creator', content: '@oseunando' },
+      { name: 'twitter:title', content: 'OneDev studio - Um estúdio de desenvolvimento web de um homem só' },
+      { name: 'twitter:description', content: 'Um estúdio de desenvolvimento web de um homem só em Curitiba Paraná, desenvolvimento JavaScript e WordPress' },
+      { name: 'twitter:image', content: '/share.png' }
     ],
     link: [
       { rel: 'me', href: 'nandomoreira.me@gmail.com' },
